c588c1ce1461f0d4b35e6dcca573a8cfffab6cc1
volca <volca@tom.com>
2016-03-12 11:49:12 +0800

    log

--- server.py
+++ server.py
@@ -3,22 +3,19 @@ from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer, SimpleSSLWeb
 from optparse import OptionParser
 
 clients = []
+i = 0
 class SimpleChat(WebSocket):
 
     def handleMessage(self):
-      print ''.join(' {:02x}'.format(x) for x in self.data)
+      print self.data
 
     def handleConnected(self):
        print self.address, 'connected'
-       for client in clients:
-          client.sendMessage(self.address[0] + u' - connected')
        clients.append(self)
 
     def handleClose(self):
        clients.remove(self)
        print self.address, 'closed'
-       for client in clients:
-          client.sendMessage(self.address[0] + u' - disconnected')
 
 server = SimpleWebSocketServer('', 8000, SimpleChat)
 server.serveforever()
